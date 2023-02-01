0058c885551c120347575c6d5db593135c5ec4aa
nikita1120 <85880660+nikita1120@users.noreply.github.com>
2023-02-01 15:00:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-vdqrwj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-vdqrwj)
---
+++ index.js
@@ -0,0 +1,18 @@
+// run `node index.js` in the terminal
+
+let i
+for(i=1;i<=100;i++){
+  if(i%15 ===0 ){
+    console.log("amazon" +" ")
+  }
+  else if (i%3 === 0){
+    console.log("google ")
+  }
+  else if (i%5===0){
+
+    console.log("facebook ")
+  }
+  else{
+    console.log(i+" ")
+  }
+}
